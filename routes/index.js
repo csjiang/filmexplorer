a5ed3a77dcf1ec2256b21d4e27cc2239311afa1c
C.S. Jiang <christine.s.jiang@gmail.com>
2016-11-14 14:37:14 -0500

    single-movie page added

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,12 @@
 const express = require('express');
 const router = express.Router();
 const filmBank = require('../filmBank');
-var omdb = require('omdb');
-
+const imdb = require('imdb-api');
+const omdb = require('omdb');
 const Promise = require ('bluebird');
 
 router.get('/', function (req, res) {
-  res.render( 'index', {showForm: true} );
+  res.render('index', {showForm: true} );
 });
 
 router.post('/search', function(req, res) {
@@ -16,15 +16,44 @@ router.post('/search', function(req, res) {
 
 router.get('/result/:name', function(req, res) {
 	omdb.search(req.params.name, function(err, movies) {
-	 		if (err) {
-	 			return new Error();
-	 		}
-	 		if (movies.length < 1) {
-	 			return console.log('No movies were found!');
-	 		}
-			res.render('index', {movies: movies});
-	 	});
+ 		if (err) {
+ 			return new Error();
+ 		}
+ 		if (movies.length < 1) {
+ 			return console.log('No movies were found!');
+ 		}
+		res.render('index', {movies: movies});
+	});
 });
+
+router.get('/result/film/:movieName/:movieId', function(req, res, next) {
+	omdb.get(req.params.movieId, {
+		fullPlot: true,
+		tomatoes: true
+	}, function(err, movie) {
+		if (err) {
+			return console.err(err);
+		} 
+		if(!movie) {
+			return console.log('Movie not found!');
+		}
+		res.render('filmpage', {movie: movie});
+	});
+});
+
+// router.get('/result/', function(req, res, next) {
+// 	omdb.search({
+// 		terms: '', 
+// 		year: req.query.year}, function(err, movies) {
+//  		if (err) {
+//  			return new Error();
+//  		}
+//  		if (movies.length < 1) {
+//  			return console.log('No movies were found!');
+//  		}
+// 		res.render('index', {movies: movies});
+// 	});
+// });
 
 //in-progress
 router.get('/result/?genre=', function(req, res) {
